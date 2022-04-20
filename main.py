cbed755b22a5f1d7d7fa492dd3e3940af3dc7472
Habiba1998 <54662472+Habiba1998@users.noreply.github.com>
2022-04-20 08:36:25 -0700

    Add command-line interface

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+#!usr/bin/python
 from moviepy.editor import VideoFileClip                                # pip install moviepy
 from pipeline import pipeline
+import sys
 
-def processVideo(videopath, resultpath):
-    frames = VideoFileClip(videopath)                                   # Read the video
-    frames.fl_image(pipeline)                                           # Call the pipeline function
-    frames.write_videofile(resultpath, audio=False)                     # Output a resulting video
+def processVideo(videopath, resultpath, debug_mode_on):
+    frames = VideoFileClip(videopath)  # Read the video
+    myframes = frames.fl_image(lambda image: pipeline(image, debug_mode_on))
+    myframes.write_videofile(resultpath, audio=False)  # Output a resulting video
     frames.reader.close()
     frames.close()
 
-i = 0                                                                   # index of the processed video
-videopath = ["project_video.mp4", "challenge_video.mp4", "harder_challenge_video.mp4"]
-resultpath = ["result1.mp4", "result2.mp4", "result3.mp4"]
-processVideo(videopath[i], resultpath[i])
+
+n = len(sys.argv)
+if n < 2:
+    print("Error: Please pass the video path as an argument")
+    sys.exit(0)
+videopath = sys.argv[1]
+if n > 2:
+    resultpath = "./"+sys.argv[2]
+else:
+    resultpath = "./result.mp4"
+# Getting the mode 0 means normal (default), 1 means debug
+if n > 3:
+    debug_mode_on = sys.argv[3]
+else:
+    debug_mode_on = 0
+
+processVideo(videopath, resultpath, debug_mode_on)
